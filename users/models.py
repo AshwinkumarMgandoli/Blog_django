11a2662f47bbf275375f0f1e9f8aa621bc920534
AshwinkumarMG <90339243+AshwinkumarMgandoli@users.noreply.github.com>
2025-03-03 11:20:37 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,13 +10,23 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        # Save the profile first
+        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        # Check if the image exists before opening it
+        try:
+            img = Image.open(self.image.path)
+        except FileNotFoundError:
+            # Handle the case where the file doesn't exist
+            # For example, you can set a default image if the file is missing
+            self.image = 'default.jpg'
+            super().save(*args, **kwargs)
+            return  # Exit to prevent further processing
 
-
+        # Resize image if it's too large
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
